b639bbe08f87e1b6dd853f6dfd1b7d9ddadf6449
fiend91206 <fiend91206@gmail.com>
2012-10-11 21:01:31 -0400

    [#35368327] forgot to add files

---
+++ mobile/javascript/ezgotv.js
@@ -0,0 +1,90 @@
+//https://gdata.youtube.com/feeds/api/users/ezgotv/uploads
+var videos = new Array();
+var numVideos = 0;
+$(document).ready(function() {
+  retrieveVideoList();
+});
+
+/**
+ * Makes an ajax call to the youtube api to retrieve a list of
+ * videos made by e-z-go
+ */
+function retrieveVideoList(){
+    $.ajax({
+        url: "https://gdata.youtube.com/feeds/api/users/ezgotv/uploads",
+        type: "get",
+        context: document.body,
+        dataType: "xml",
+        success: function(xml){
+            $(xml).find("entry").each(function(){
+                var id = $(this).find("id").text();
+                var title = $(this).find("title").text();
+                var content = $(this).find("content").text();
+                var mlink = "";
+                $(this).find("link").each(function(){
+                    var index = $(this).attr('rel').indexOf('#') + 1;
+                    var str = $(this).attr('rel').substring(index);
+                    if (str == "mobile"){
+                        mlink = $(this).attr("href");
+                    }
+                });
+                var i = 0;
+                var thumbnail = "";
+                var height = 0, width = 0;
+                //This will only work on chrome
+                $(this).find("group").find("thumbnail").each(function(){
+                    if (i == 2){
+                        thumbnail = $(this).attr("url");
+                        height = $(this).attr("height");
+                        width = $(this).attr("width");
+                    }else {
+                        i++;
+                    }
+                });
+                var vid = new Video(id, title, content, mlink, thumbnail, height, width);
+                videos[numVideos++] = vid;
+            });
+            printVideos();
+        },
+        error: function(jqXHR, textStatus, errorThrown){
+            // log the error to the console
+            console.log(
+                "The following error occured: "+
+                textStatus, errorThrown
+            );
+        }
+    });
+}
+
+/**
+ * object that represents a single video
+ */
+function Video(id, title, content, mlink, thumbnail, height, width){
+    this.id = id;
+    this.title = title;
+    this.content = content;
+    this.mlink = mlink;
+    this.thumbnail = thumbnail;
+    this.height = height;
+    this.width = width;
+}
+
+/**
+ * Will write a thumbnail image and link onto the page for a user to click on
+ */
+function printVideos(){
+    var table = '<table>\n';
+    for(var i = 0; i < videos.length; i++){
+        var str = '<tr><td>\n'
+        str += '<a href="' + videos[i].mlink +'">\n';
+        str += '<img src="' + videos[i].thumbnail +'"/></a></td>\n';
+        str += '<td><a href="' + videos[i].mlink +'">\n';
+        str += videos[i].title + '\n';
+        str +=  '</a></td></tr>\n';
+        table  += str;
+    }
+    table += '</table>\n';
+    $('.videos').append(table);
+    
+    //
+}
---
+++ mobile/javascript/findDealer.js
@@ -0,0 +1,73 @@
+/*
+*	This function initializes the google map
+*	It centers the map at Georgia Tech and zooms it to show mainly the campus.
+*/
+function initialize() {
+	 var myOptions = {
+	 zoom: 15,
+	 center: new google.maps.LatLng(33.777417,-84.397252),		// Center at GA Tech
+	 mapTypeId: google.maps.MapTypeId.ROADMAP
+	 }
+	 var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);	// Grabs the map element
+	 
+	 var i, marker, printers = window.printers;
+	 var infowindow = new google.maps.InfoWindow();
+	 
+	 for (i = 0; i < printers.length; i++){
+		 marker = new google.maps.Marker({
+		 	position: new google.maps.LatLng(printers[i].latitude,printers[i].longitude),
+		  	map: map,
+		 	title:printers[i].name
+		 });
+		
+		 google.maps.event.addListener(marker, 'click', (function(marker, i) {
+			 return function(){
+				 infowindow.setContent(
+				 	'<div id="content">'+
+					'<div id="siteNotice">'+ '</div>'+
+					'<h2 id="firstHeading" class="firstHeading">'+ printers[i].building + '</h2>'+
+					'<div id="bodyContent">'+
+						'<p>Printer Type: ' + printers[i].printer_type + '</p>' +  
+						'<p>Location: ' + printers[i].location + '</p>' + 
+						'<p>Uses Free Printer Funds: ' + printers[i].free + '</p>' + 
+					'</div>'+
+					'</div>');
+				 infowindow.open(map,marker);
+			 }
+		 })(marker, i));
+	 }
+}
+	
+	
+/*
+*	This function creates the google map with our key
+*/
+function loadScript() {
+	var script = document.createElement("script");
+	script.type = "text/javascript";
+	script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyBMtjbKML2WeqX7NdNSJWEy06_t7M1DzTU&sensor=false&callback=initialize";
+	document.body.appendChild(script);
+}
+
+/*
+*	This function queries the printer database and returns an array of all the printer data
+*/
+function getPrinters() {
+    var printerData;
+    $.ajax({
+       url: "../../api/david/printer_locations",	// Location of database functions
+	    context: document.body,
+        success: function(data)				// Returns data if database was sucesfully queried
+        {
+			window.printers = jQuery.parseJSON(data);
+			loadScript();
+        }
+    });
+}
+
+/*
+*	Once the page is fully loaded we will build the map
+*/
+$(document).ready(function() {
+   loadScript();
+});
