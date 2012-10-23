a6d5fc0b6d584fb9ccda51def3db25f341d55bc0
fiend91206 <fiend91206@gmail.com>
2012-10-23 16:11:06 -0400

    Added code to sanitize user input before a call to the database

--- db/index.php
+++ db/index.php
@@ -16,143 +16,146 @@ if (isset($request['command'])){
     $command = $request['command'];
     if(strcasecmp($command, 'authenticate') == 0){
         if (isset($request['username']) && isset($request['password'])){
-            $username = $request['username'];
-            $password = $request['password'];
+            $username =  mysql_real_escape_string($request['username']);
+            $password = mysql_real_escape_string($request['password']);
             authenticate($username, $password);
         }
     }else if (strcasecmp($command, 'findDealers') == 0){
 	
 	if (isset($request['lat']) && isset($request['lng']) && isset($request['zoom'])){
-	    findDealers($request['lat'], $request['lng'], $request['zoom']);
+	    $lat = mysql_real_escape_string($request['lat']);
+	    $lng = mysql_real_escape_string($request['lng']);
+	    $zoom = mysql_real_escape_string($request['zoom']);
+	    findDealers($lat, $lng, $zoom);
 	}
     } else if(strcasecmp($command, 'serialsearch') == 0){
         if (isset($request['serialNumber'])) {
-            serialSearch($request['serialNumber']);
+            serialSearch(mysql_real_escape_string($request['serialNumber']));
         }
     } else if(strcasecmp($command, 'selectVehicleResults') == 0){
         if (isset($request['model']) && isset($request['fuel']) && isset($request['submodel']) && isset($request['year'])) {
-            $model = $request['model'];
-            $fuel = $request['fuel'];
-            $submodel = $request['submodel'];
-            $year = $request['year'];
+            $model = mysql_real_escape_string($request['model']);
+            $fuel = mysql_real_escape_string($request['fuel']);
+            $submodel = mysql_real_escape_string($request['submodel']);
+            $year = mysql_real_escape_string($request['year']);
             selectVehicleResults($model, $fuel, $submodel, $year);
         }
     } else if(strcasecmp($command, 'selectModelLoad') == 0){
     	selectModelLoad();
     } else if(strcasecmp($command, 'selectFuelLoad') == 0){
 	if(isset($request['model'])) {
-            $model = $request['model'];
+            $model = mysql_real_escape_string($request['model']);
             selectFuelLoad($model);
 	}
     } else if(strcasecmp($command, 'selectSubmodelLoad') == 0){
 	if(isset($request['model']) && isset($request['fuel'])) {
-            $model = $request['model'];
-	    $fuel = $request['fuel'];
+            $model = mysql_real_escape_string($request['model']);
+	    $fuel = mysql_real_escape_string($request['fuel']);
             selectSubmodelLoad($model, $fuel);
 	}
     } else if(strcasecmp($command, 'selectYearLoad') == 0){
 	if(isset($request['model']) && isset($request['fuel']) && isset($request['submodel'])) {
-            $model = $request['model'];
-	    $fuel = $request['fuel'];
-	    $submodel = $request['submodel'];
+            $model = mysql_real_escape_string($request['model']);
+	    $fuel = mysql_real_escape_string($request['fuel']);
+	    $submodel = mysql_real_escape_string($request['submodel']);
             selectYearLoad($model, $fuel, $submodel);
 	}
     } else if(strcasecmp($command, 'accountVehiclesLoad') == 0){
 	if(isset($request['account'])) {
-            $account = $request['account'];
+            $account = mysql_real_escape_string($request['account']);
 	    accountVehiclesLoad($account);
 	}
     } else if(strcasecmp($command, 'partsSearch') == 0){
 	if(isset($request['query'])) {
-            $search = $request['query'];
+            $search = mysql_real_escape_string($request['query']);
 	    partsSearch($search);
 	}
     } else if(strcasecmp($command, 'partCategoryLoadAll') == 0){
     	partCategoryLoadAll();
     } else if(strcasecmp($command, 'partCategoryLoadFilter') == 0){
 	if(isset($request['filter'])) {
-            $filter = $request['filter'];
+            $filter = mysql_real_escape_string($request['filter']);
 	    partCategoryLoadFilter($filter);
 	}
     } else if(strcasecmp($command, 'selectPartResultsAll') == 0){
 	if(isset($request['subcategory'])) {
-            $subcategory = $request['subcategory'];
+            $subcategory = mysql_real_escape_string($request['subcategory']);
 	    selectPartResultsAll($subcategory);
 	}
     } else if(strcasecmp($command, 'selectPartResultsFiltered') == 0){
 	if(isset($request['subcategory']) && isset($request['filter'])) {
-            $subcategory = $request['subcategory'];
-            $filter = $request['filter'];
+            $subcategory = mysql_real_escape_string($request['subcategory']);
+            $filter = mysql_real_escape_string($request['filter']);
 	    selectPartResultsFiltered($subcategory, $filter);
 	}
     } else if(strcasecmp($command, 'partInfoLoad') == 0){
 	if(isset($request['part'])) {
-            $part = $request['part'];
+            $part = mysql_real_escape_string($request['part']);
 	    partInfoLoad($part);
 	}
     } else if(strcasecmp($command, 'partSubcategoryLoadAll') == 0){
 	if(isset($request['category'])) {
-            $category = $request['category'];
+            $category = mysql_real_escape_string($request['category']);
 	    partSubcategoryLoadAll($category);
 	}
     } else if(strcasecmp($command, 'partSubcategoryLoadFilter') == 0){
 	if(isset($request['category']) && isset($request['filter'])) {
-            $category = $request['category'];
-            $filter = $request['filter'];
+            $category = mysql_real_escape_string($request['category']);
+            $filter = mysql_real_escape_string($request['filter']);
 	    partSubcategoryLoadFilter($category, $filter);
 	}
     } else if(strcasecmp($command, 'vehicleResultsAccountSave') == 0){
 	if(isset($request['account']) && isset($request['serial'])) {
-            $account = $request['account'];
-            $serial = $request['serial'];
+            $account = mysql_real_escape_string($request['account']);
+            $serial = mysql_real_escape_string($request['serial']);
 	    vehicleResultsAccountSave($account, $serial);
 	}
     } else if(strcasecmp($command, 'accountVehicleRemove') == 0){
 	if(isset($request['account']) && isset($request['serial'])) {
-            $account = $request['account'];
-            $serial = $request['serial'];
+            $account = mysql_real_escape_string($request['account']);
+            $serial = mysql_real_escape_string($request['serial']);
 	    accountVehicleRemove($account, $serial);
 	}
     } else if(strcasecmp($command, 'shoppingCartLoad') == 0){
 	if(isset($request['account'])) {
-            $account = $request['account'];
+            $account = mysql_real_escape_string($request['account']);
 	    shoppingCartLoad($account);
 	}
     } else if(strcasecmp($command, 'addToCart') == 0){
 	if(isset($request['account']) && isset($request['part'])) {
-            $account = $request['account'];
-            $part = $request['part'];
+            $account = mysql_real_escape_string($request['account']);
+            $part = mysql_real_escape_string($request['part']);
 	    addToCart($account, $part);
 	}
     } else if(strcasecmp($command, 'shoppingCartRemove') == 0){
 	if(isset($request['account']) && isset($request['part'])) {
-            $account = $request['account'];
-            $part = $request['part'];
+            $account = mysql_real_escape_string($request['account']);
+            $part = mysql_real_escape_string($request['part']);
 	    shoppingCartRemove($account, $part);
 	}
     } else if(strcasecmp($command, 'checkout') == 0){
 	if(isset($request['account'])) {
-            $account = $request['account'];
+            $account = mysql_real_escape_string($request['account']);
 	    checkout($account);
 	}
     } else if(strcasecmp($command, 'loadAccountInfo') == 0){
 	if(isset($request['account'])) {
-            $account = $request['account'];
+            $account = mysql_real_escape_string($request['account']);
 	    loadAccountInfo($account);
 	}
     } else if(strcasecmp($command, 'updateAccountSamePassword') == 0){
 	if(isset($request['email']) && isset($request['offers']) && isset($request['account'])) {
-            $email = $request['email'];
-	    $offers = $request['offers'];
-	    $account = $request['account'];
+            $email = mysql_real_escape_string($request['email']);
+	    $offers = mysql_real_escape_string($request['offers']);
+	    $account = mysql_real_escape_string($request['account']);
             updateAccountSamePassword($email, $offers, $account);
 	}
     } else if(strcasecmp($command, 'updateAccountNewPassword') == 0){
 	if(isset($request['email']) && isset($request['password']) && isset($request['offers']) && isset($request['account'])) {
-            $email = $request['email'];
-	    $password = $request['password'];
-	    $offers = $request['offers'];
-	    $account = $request['account'];
+            $email = mysql_real_escape_string($request['email']);
+	    $password = mysql_real_escape_string($request['password']);
+	    $offers = mysql_real_escape_string($request['offers']);
+	    $account = mysql_real_escape_string($request['account']);
             updateAccountNewPassword($email, $password, $offers, $account);
 	}
     }
