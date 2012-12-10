ee91353ee436096885061c947ada0bd260e3eac3
TKRKS <TKRKSN@gmail.com>
2012-12-09 20:10:13 -0500

    changed total and registration error

--- javascript/shoppingCart.js
+++ javascript/shoppingCart.js
@@ -8,19 +8,27 @@ $.ajax({
 			//Display the parts information
 			var parts = response.data;
 			var sum = 0;
-			var string = '<div id = "productList" name = "productList" >';
-			string = string + "Items in Cart: </br></br>";
-			$.each(parts, function(index, value) {
-				if (value.image != null) {
-					string = string + '<img src = "' + value.image + '"/><br/>';
-				}
-				string = string + 'Name: ' + value.name + '<br/>Price: '+ value.price;
-				string = string + '<button onclick="removePart(value)" value="' + value.partNumber + '">Remove Part</button><br/>';
-				sum = sum + parseFloat(value.price);
-			});
-			string = string + '</div>';
-			$('#totalCost').replaceWith('<div id = "totalCost" name = "totalCost" >' + "Total: " + sum + "</div>");
-			$('#totalCost').trigger('create');
+			if (parts.length > 0) {
+				var string = '<div id = "productList" name = "productList" >';
+				string = string + '<div id = "totalCost" name = "totalCost" style = "font-size:200%;font-weight:bold;text-align:center">' + "Total: sumGoesHere </div>" + '<br/><button onClick="checkout()">Checkout</button>' + "<br/><br/>";
+				string = string + "Items in Cart: <br/><br/>";
+				$.each(parts, function(index, value) {
+					if (value.image != null) {
+						string = string + '<img src = "' + value.image + '"/><br/>';
+					}
+					string = string + 'Name: ' + value.name + '<br/>Price: '+ value.price;
+					string = string + '<button onclick="removePart(value)" value="' + value.partNumber + '">Remove Part</button><br/>';
+					sum = sum + parseFloat(value.price);
+				});
+				string = string + '</div>';
+				string = string.replace("sumGoesHere", "$" + sum);
+			} else {
+				var string = '<div id = "productList" name = "productList" >' + 'No items currently in the cart.' + '</div>';
+			}			
+			$('#productList').replaceWith(string);
+			$('#productList').trigger('create');
+		} else {
+			var string = '<div id = "productList" name = "productList" >' + 'No items currently in the cart.' + '</div>';
 			$('#productList').replaceWith(string);
 			$('#productList').trigger('create');
 		}
